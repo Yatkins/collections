d9c0336033924c09aa78f441460f295466eb1f11
Yatkins <tzippi8@gmail.com>
2020-05-08 00:34:02 -0500

    fixed FileToArrayList

--- src/main/java/edu/ti/caih313/collections/hw9/FileToArrayList.java
+++ src/main/java/edu/ti/caih313/collections/hw9/FileToArrayList.java
@@ -7,45 +7,57 @@ import java.util.Scanner;
 
 public class FileToArrayList {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         File file = new File("src/main/resources/inputSentences");
         System.out.println("I will read from the file " + file);
         System.out.println(fileToArray(file));
     }
 
-    private static String fileToArray(File file){
+    private static String fileToArray(File file) {
         Scanner input = null;
         ArrayList<ArrayList<String>> wholeFile = new ArrayList<>();
-        String index = "";
         try {
             input = new Scanner(file);
             while (input.hasNextLine()) {
                 ArrayList<String> sentence = new ArrayList<>();
-                while (input.hasNext()) {
-                    sentence.add(input.next());
-                    wholeFile.add(sentence);
+                String[] words = input.nextLine().split(" ");
+                for (int i = 0; i < words.length; i++) {
+                    sentence.add(words[i]);
                 }
+                wholeFile.add(sentence);
             }
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
-        }finally{
-            if(input != null){
+        } finally {
+            if (input != null) {
                 input.close();
             }
-            System.out.println("Enter a sentence number and a word number");
-            System.out.println("I will give you the word that occurs at that index");
-            Scanner keyboard = new Scanner(System.in);
-            int sentenceNum = keyboard.nextInt();
-            int wordNum = keyboard.nextInt();
-            ArrayList<String> askedSentence = (ArrayList<String>) wholeFile.get(sentenceNum);
-            index = askedSentence.get(wordNum);
-        }return index;
+        }
+        return stringAtIndex(wholeFile);
+    }
+
+    public static String stringAtIndex(ArrayList<ArrayList<String>> wholeFile) {
+        System.out.println("Enter a sentence number and a word number");
+        System.out.println("I will give you the word that occurs at that index");
+        Scanner keyboard = new Scanner(System.in);
+        int sentenceNum = keyboard.nextInt();
+        int wordNum = keyboard.nextInt();
+        boolean valid = isValid(sentenceNum-1, wordNum -1, wholeFile);
+        if (valid) {
+            ArrayList<String> askedSentence = (ArrayList<String>) wholeFile.get(sentenceNum - 1);
+            String index = askedSentence.get(wordNum - 1);
+            return index;
+        } else {
+            return "You entered an index that doesn't exist.";
+        }
+    }
+
+
+    public static boolean isValid(int sentenceNum, int wordNum, ArrayList<ArrayList<String>> wholeFile) {
+        boolean isValid = true;
+        if ((sentenceNum > wholeFile.size()) || (wordNum > wholeFile.get(sentenceNum).size())
+                || sentenceNum == 0 || wordNum == 0) {
+            isValid = false;
+        }return isValid;
     }
 }
-    //Write a program that will read sentences from a text file,
-// placing each sentence in its own instance of ArrayList.
-// (You will create a sentence object by adding words to it one at a time as they are read.)
-// When a sentence has been completely read, add the sentence to another instance of ArrayList.
-// Once you have read the entire file, you will have an instance of ArrayList that contains several instances of ArrayList,
-
-// If the sentence number or word number is not valid, provide an appropriate error message.
